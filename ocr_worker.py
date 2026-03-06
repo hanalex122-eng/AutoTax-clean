46c45be947cd3019663608d1a517be4a23eaa151
hanalex122-eng <hanalex122@gmail.com>
2026-03-06 23:01:41 +0100

    update OCR worker pipeline

--- ocr_worker.py
+++ ocr_worker.py
@@ -1,19 +1,44 @@
-import multiprocessing
-multiprocessing.set_start_method("spawn", force=True)
-
-
+import os
 from paddleocr import PaddleOCR
-import cv2
 
-ocr = PaddleOCR(use_angle_cls=True, lang="en")
+# OCR model
+ocr = PaddleOCR(
+    use_textline_orientation=True,
+    lang="en"
+)
 
 def process_image(image_path):
 
     result = ocr.ocr(image_path)
 
     texts = []
+
     for line in result:
         for word in line:
             texts.append(word[1][0])
 
     return " ".join(texts)
+
+
+def process_folder(folder):
+
+    files = os.listdir(folder)
+
+    for file in files:
+
+        if file.lower().endswith((".png", ".jpg", ".jpeg")):
+
+            path = os.path.join(folder, file)
+
+            print("\n====================")
+            print("FILE:", file)
+            print("====================")
+
+            text = process_image(path)
+
+            print(text)
+
+
+if __name__ == "__main__":
+
+    process_folder(r"C:\Users\alex-\Desktop\fatura")
